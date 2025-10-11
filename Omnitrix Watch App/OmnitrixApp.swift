d410a08f8f55871d0f31165666438349cc7dc9af
enesgules <abdullah.enes.gules@gmail.com>
2025-10-11 16:33:47 +0300

    Initial Commit

---
+++ Omnitrix Watch App/OmnitrixApp.swift
@@ -0,0 +1,17 @@
+//
+//  OmnitrixApp.swift
+//  Omnitrix Watch App
+//
+//  Created by Abdullah Enes Güleş on 11.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct Omnitrix_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
